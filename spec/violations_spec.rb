85cff97b3b8bb5f0508fffa07e1d22804ad3b7f4
Joe Jobes <jmrjobes@gmail.com>
2018-02-20 09:26:32 -0700

    Add class method test for listing all violations

--- spec/violations_spec.rb
+++ spec/violations_spec.rb
@@ -2,6 +2,10 @@ require 'rspec'
 
 describe "when count_violations is run" do
   it "returns count of violations" do
-    expect(Violations.count_violations).to eq()
+    expect(ViolationAnalytics.count_violations).to eq()
+  end
+  it "prints out all violation types without duplication" do
+
+    expect(ViolationAnalytics.violation_types.count).to eq(0)
   end
 end
