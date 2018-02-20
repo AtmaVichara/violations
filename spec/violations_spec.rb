bd2cb333b73f01f66fe10f37fecee328ac2f164d
Joe Jobes <jmrjobes@gmail.com>
2018-02-20 09:37:07 -0700

    Add assertions for count_violations and violations_type

--- spec/violations_spec.rb
+++ spec/violations_spec.rb
@@ -1,11 +1,17 @@
 require 'rspec'
+require './lib/violation_analytics.rb'
+require './lib/violations.rb'
 
 describe "when count_violations is run" do
+  before(:each) do
+    ViolationAnalytics.parse_csv
+  end
   it "returns count of violations" do
-    expect(ViolationAnalytics.count_violations).to eq()
+    expect(ViolationAnalytics.count_violations.count).to eq(43)
+    expect(ViolationAnalytics.count_violations).to include("Unsanitary conditions, not specified: 74")
   end
   it "prints out all violation types without duplication" do
 
-    expect(ViolationAnalytics.violation_types.count).to eq(0)
+    expect(ViolationAnalytics.violation_types.count).to eq(43)
   end
 end
