c7fd11605676e9eaed881ddc9734c3897cdf4d4a
Joe Jobes <jmrjobes@gmail.com>
2018-02-20 09:54:03 -0700

    Add test for grabbing earlist dates

--- spec/violations_spec.rb
+++ spec/violations_spec.rb
@@ -10,8 +10,14 @@ describe "when count_violations is run" do
     expect(ViolationAnalytics.count_violations.count).to eq(43)
     expect(ViolationAnalytics.count_violations).to include("Unsanitary conditions, not specified: 74")
   end
-  it "prints out all violation types without duplication" do
-
+  it "grabs all violation types without duplication" do
     expect(ViolationAnalytics.violation_types.count).to eq(43)
+  end
+  it "prints out violations and counts" do
+    expect(ViolationAnalytics.print_violation_counts.class).to eq(Array)
+  end
+  it "grabs earlist date for each violation" do
+    expect(ViolationAnalytics.earliest_date.count).to eq(43)
+    expect(ViolationAnalytics.earliest_date.first).to eq("Refuse Accumulation: Earliest Date: 2012-01-03 00:00:00")
   end
 end
